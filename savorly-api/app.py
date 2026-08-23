092f22a9cdf32229d82bb5ddbb5fe073e02b34b7
Santhosh0289 <ssanthosh0289@gmail.com>
2026-08-23 12:47:58 +0530

    changes in cors

--- savorly-api/app.py
+++ savorly-api/app.py
@@ -15,8 +15,7 @@ def create_app():
     jwt.init_app(app)
     cors.init_app(app, resources={r"/api/*": {"origins": [
         "http://localhost:5173",
-        "https://savorly-santhosh-s-projects-9d334406.vercel.app",
-        r"https://savorly-.*\.vercel\.app"
+        "https://savorly-1.onrender.com"
     ]}})
 
     app.register_blueprint(auth_bp, url_prefix="/api/auth")
